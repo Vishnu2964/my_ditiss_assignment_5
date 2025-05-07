7cf282d77bc766abe50f91dea9ee6f7f05999435
Vishnu2964 <74960269+Vishnu2964@users.noreply.github.com>
2025-05-07 20:19:54 +0530

    Added third print line

--- HelloStudent.java
+++ HelloStudent.java
@@ -3,5 +3,6 @@ public class HelloStudent {
         String name = "Vishnu & Sahal";
         System.out.println("Hello, " + name + "! Welcome to Java programming.");
 	System.out.println("Welcome to Pune C-DAC");
+	System.out.println("Welcome to DITISS");
     }
 }
